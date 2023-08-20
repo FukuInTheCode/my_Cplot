2dab389f30e45b3fed0e10912566fd5e3f167912
FukuInTheCode <tomc.code@gmail.com>
2023-08-21 01:26:12 +0200

    add my_colors_t

--- includes/my_plot.h
+++ includes/my_plot.h
@@ -4,7 +4,14 @@
 #include "../includes/SFML/Graphics.h"
 #include "../includes/SFML/Window.h"
 
+typedef struct my_colors my_colors_t;
+
 typedef struct my_fig {
     sfWindow *window;
     sfEvent *event;
+    my_colors_t *colors;
 } my_fig_t;
+
+typedef struct my_colors {
+    sfColor background;
+} my_colors_t;
