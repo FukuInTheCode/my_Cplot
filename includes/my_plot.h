418f8fe55e3909c51e5d88302bae863eb3b296ce
FukuInTheCode <tomc.code@gmail.com>
2023-08-21 23:09:31 +0200

    my_plot struct

--- includes/my_plot.h
+++ includes/my_plot.h
@@ -7,17 +7,29 @@
 #define SCALE 2
 
 typedef struct my_colors my_colors_t;
+typedef struct my_plot my_plot_t;
 
 typedef struct my_fig {
     sfRenderWindow *window;
     char *title;
     sfEvent *event;
     my_colors_t *colors;
+    my_plot_t *plot;
 } my_fig_t;
 
 typedef struct my_colors {
     sfColor background;
 } my_colors_t;
+
+typedef struct my_plot {
+    int32_t *xs;
+    int32_t *ys;
+    uint32_t num;
+    int32_t max_x;
+    int32_t min_x;
+    int32_t max_y;
+    int32_t min_y;
+} my_plot_t;
 
 void my_fig_create(my_fig_t *fig);
 void my_fig_show(my_fig_t *fig);
--- src/main.c
+++ src/main.c
@@ -3,6 +3,13 @@
 int main(void)
 {
     my_colors_t colors = {.background = sfBlack};
+    int32_t x[] = {1, 2, 3};
+    int32_t y[] = {3, 2, 1};
+    my_plot_t plot = {.xs = x, .ys = y, .num = 3};
+
+    for (uint8_t i = 0; i < plot.num; i++)
+        printf("%d, %d\n", (plot.xs)[i], (plot.ys)[i]);
+
     char *title = "test";
     sfEvent event;
     my_fig_t fig1 = {.title = title, .colors = &colors, .event = &event};
