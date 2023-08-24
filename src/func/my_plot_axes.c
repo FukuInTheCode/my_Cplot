7063d847d1c5b0c6ddccca3f5d7307195406e82b
FukuInTheCode <tomc.code@gmail.com>
2023-08-24 21:21:11 +0200

    positive x-axis graduations

--- src/func/my_plot_axes.c
+++ src/func/my_plot_axes.c
@@ -1,9 +1,19 @@
 #include "../../includes/my.h"
 
-// static void my_plot_axes_graduation(my_fig_t *fig)
-// {
+static void my_plot_axes_graduation_horizontal(my_fig_t *fig)
+{
+    sfVector2u tmp_vec = sfRenderWindow_getSize(fig->window);
+    double steps = fig->plot->max_x / 10;
+    size_t i = fig->plot->axe2 + steps;
+    for ( ; i < tmp_vec.y && i != fig->plot->axe2; i += steps) {
+        sfVertex line[] = {
+        {{i, fig->plot->axe1 - 5}, sfWhite},
+        {{i, fig->plot->axe1 + 5}, sfWhite}
+        };
 
-// }
+    sfRenderWindow_drawPrimitives(fig->window, line, 2, sfLines, NULL);
+    }
+}
 
 void my_plot_axes(my_fig_t *fig)
 {
@@ -20,5 +30,5 @@ void my_plot_axes(my_fig_t *fig)
     };
 
     sfRenderWindow_drawPrimitives(fig->window, line2, 2, sfLines, NULL);
-    // my_plot_axes_graduation(fig);
+    my_plot_axes_graduation_horizontal(fig);
 }
