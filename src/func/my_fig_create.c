375a777a30c138fddcef59b860611ae53d54b9f0
FukuInTheCode <tomc.code@gmail.com>
2023-08-25 17:56:57 +0200

    clean plot pts

--- src/func/my_fig_create.c
+++ src/func/my_fig_create.c
@@ -2,7 +2,7 @@
 
 static void create_window(my_fig_t *fig)
 {
-    volatile sfVideoMode mode = {500 * SCALE, 500 * SCALE, 32};
+    sfVideoMode mode = {500 * SCALE, 500 * SCALE, 32};
     fig->window = sfRenderWindow_create(mode, fig->title, \
         sfClose, NULL);
 }
--- src/func/my_plot_mouse.c
+++ src/func/my_plot_mouse.c
@@ -6,6 +6,7 @@ void my_plot_handle_mouse(my_fig_t *fig)
     fig->plot->ver_shift = fig->shift_save.y;
     sfVector2i mouse_vec = sfMouse_getPosition(fig->window);
     sfMouseButton left_btn = sfMouseLeft;
+
     if (fig->is_moving == sfFalse && sfMouse_isButtonPressed(left_btn)) {
         fig->is_moving = sfTrue;
         fig->mouse_vec_save.x = mouse_vec.x;
@@ -14,6 +15,7 @@ void my_plot_handle_mouse(my_fig_t *fig)
         fig->plot->hor_shift -= fig->mouse_vec_save.x - mouse_vec.x;
         fig->plot->ver_shift += fig->mouse_vec_save.y - mouse_vec.y;
     }
+
     if (!sfMouse_isButtonPressed(left_btn) && fig->is_moving == sfTrue) {
         fig->is_moving = sfFalse;
         fig->shift_save.x = fig->plot->hor_shift;
