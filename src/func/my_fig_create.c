e840292958d7aea935cada7d417d6941b11ceb04
FukuInTheCode <tomc.code@gmail.com>
2023-08-25 22:09:06 +0200

    changes in my_math headers

--- src/func/my_fig_create.c
+++ src/func/my_fig_create.c
@@ -7,17 +7,28 @@ static void create_window(my_fig_t *fig)
         sfClose, NULL);
 }
 
-static void calc_ratio_x(my_fig_t *fig)
+static void calc_ratio(my_fig_t *fig)
 {
-    my_fig_set_exvalues(fig);
     sfVector2u tmp_vec = sfRenderWindow_getSize(fig->window);
-    double min_x = fig->plot->min_x;
+    if (fig->plot->type != function_pts) {
+        my_fig_set_exvalues(fig);
+        double min_x = fig->plot->min_x;
 
-    fig->plot->ratio.x = tmp_vec.x - fig->ui->point_radius * 2 -\
-                            (min_x != 0 && fig->plot->max_x != 0 ? 0 : 40);
+        fig->plot->ratio.x = tmp_vec.x - fig->ui->point_radius * 2 -\
+                                (min_x != 0 && fig->plot->max_x != 0 ? 0 : 40);
 
-    fig->plot->ratio.x /= fig->plot->max_x - min_x;
+        fig->plot->ratio.x /= fig->plot->max_x - min_x;
 
+        double min_y = fig->plot->min_y;
+        fig->plot->ratio.y = tmp_vec.y - fig->ui->point_radius * 3 -\
+                            (min_y != 0 && fig->plot->max_y != 0 ? 0 : 40);
+        fig->plot->ratio.y /= fig->plot->max_y - min_y;
+    } else {
+        fig->plot->ratio.x = tmp_vec.x - fig->ui->point_radius * 2;
+        fig->plot->ratio.x /= 20;
+        fig->plot->ratio.x = tmp_vec.y - fig->ui->point_radius * 2;
+        fig->plot->ratio.x /= ;
+    }
 }
 
 static void init_variable(my_fig_t *fig)
@@ -30,7 +41,7 @@ static void init_variable(my_fig_t *fig)
     fig->plot->shift.x = 0;
     fig->plot->shift.y = 0;
 
-    calc_ratio_x(fig);
+    calc_ratio(fig);
 }
 
 void my_fig_create(my_fig_t *fig)
