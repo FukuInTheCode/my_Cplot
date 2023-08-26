03e11410c5f00230ff29c2225596bacad8b00921
FukuInTheCode <tomc.code@gmail.com>
2023-08-26 17:21:48 +0200

    now the axis are not plot unless on the screen

--- src/func/my_plot_axis.c
+++ src/func/my_plot_axis.c
@@ -7,10 +7,13 @@ void my_plot_axis(my_plot_t *plt)
         {{0, plt->graph->shift.y + tmp_vec.y / 2}, plt->theme->axis},
         {{tmp_vec.x, plt->graph->shift.y + tmp_vec.y / 2}, plt->theme->axis}
     };
-    sfRenderWindow_drawPrimitives(plt->window, line, 2, sfLines, NULL);
+    if (my_plot_is_onscreen(plt, line[0].position, axis))
+        sfRenderWindow_drawPrimitives(plt->window, line, 2, sfLines, NULL);
+    
     sfVertex line2[] = {
         {{tmp_vec.x / 2 + plt->graph->shift.x, 0}, plt->theme->axis},
         {{tmp_vec.x / 2 + plt->graph->shift.x, tmp_vec.y}, plt->theme->axis}
     };
-    sfRenderWindow_drawPrimitives(plt->window, line2, 2, sfLines, NULL);
+    if (my_plot_is_onscreen(plt, line2[0].position, axis))
+        sfRenderWindow_drawPrimitives(plt->window, line2, 2, sfLines, NULL);
 }
