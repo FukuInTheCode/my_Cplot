d410048b8b09fcf46ee4f9b8eac814f62968de6d
FukuInTheCode <tomc.code@gmail.com>
2023-08-26 02:33:05 +0200

    event and mouse are now in theire seperated c files

--- src/main.c
+++ src/main.c
@@ -49,87 +49,57 @@ int main(void)
     for (int i = 0; i < plt.graph->data_num; i++) {
         printf("{%.2f, %.2f},\n", plt.graph->points[i].x, plt.graph->points[i].y);
     }
-#if 0
-    // sfVector2u tmp_vec = sfRenderWindow_getSize(plt.window);
 
-    // for (size_t i = 0; i < ARRAY_LENGTH(points); ++i) {
-    //     if (points[i].x > max_values.x) max_values.x = points[i].x;
-    //     if (points[i].y > max_values.y) max_values.y = points[i].y;
-    //     if (points[i].x < min_values.x) min_values.x = points[i].x;
-    //     if (points[i].y < min_values.y) min_values.y = points[i].y;
+    // while (sfRenderWindow_isOpen(plt.window)) {
+    //     while (sfRenderWindow_pollEvent(plt.window, plt.event)) {
+    //         if (plt.event->type == sfEvtClosed)
+    //             sfRenderWindow_close(plt.window);
+    //     }
+
+    //     if (sfMouse_isButtonPressed(sfMouseLeft)) {
+    //         if (!is_graph_drag) {
+    //             is_graph_drag = sfTrue;
+    //             last_shift = sfMouse_getPosition(plt.window);
+    //         }
+    //         else {
+    //             sfVector2i mouse_vec = sfMouse_getPosition(plt.window);
+    //             shift.x += mouse_vec.x - last_shift.x;
+    //             shift.y += mouse_vec.y - last_shift.y;
+    //             last_shift = mouse_vec;
+    //         }
+    //     } else
+    //         is_graph_drag = sfFalse;
+
+    //     sfRenderWindow_clear(plt.window, plt.theme->bg);
+    //     for (size_t i = 0; i < ARRAY_LENGTH(points); ++i) {
+    //         sfCircleShape *current_pts = sfCircleShape_create();
+    //         sfCircleShape_setFillColor(current_pts, plt.theme->pt);
+    //         sfCircleShape_setRadius(current_pts, plt.theme->radius);
+    //         sfVector2f tmp_pts = points[i];
+    //         tmp_pts.x += shift.x;
+    //         tmp_pts.y += shift.y;
+    //         sfCircleShape_setPosition(current_pts, tmp_pts);
+    //         sfRenderWindow_drawCircleShape(plt.window, current_pts, NULL);
+    //         sfCircleShape_destroy(current_pts);
+    //     }
+    //     sfVertex line[] = {
+    //         {{0, shift.y + tmp_vec.y / 2}, plt.theme->axis},
+    //         {{tmp_vec.x, shift.y + tmp_vec.y / 2}, plt.theme->axis}
+    //     };
+
+    //     sfRenderWindow_drawPrimitives(plt.window, line, 2, sfLines, NULL);
+    //     sfVertex line2[] = {
+    //         {{tmp_vec.x / 2 + shift.x, 0}, plt.theme->axis},
+    //         {{tmp_vec.x / 2 + shift.x, tmp_vec.y}, plt.theme->axis}
+    //     };
+
+    //     sfRenderWindow_drawPrimitives(plt.window, line2, 2, sfLines, NULL);
+    //     sfRenderWindow_display(plt.window);
     // }
-
-    // sfVector2f interval = {
-    //     max_values.x - min_values.x,
-    //     max_values.y - min_values.y,
-    // };
-    // sfVector2f ratio = {
-    //     (tmp_vec.x - plt.theme->radius*2) / interval.x,
-    //     (tmp_vec.y - plt.theme->radius*2) / interval.y
-    // };
-    // for (size_t i = 0; i < ARRAY_LENGTH(points); ++i) {
-    //     points[i].x *= ratio.x;
-    //     points[i].y *= ratio.y;
-    //     points[i].y = (tmp_vec.y - plt.theme->radius*2) - points[i].y;
-    //     points[i].x += tmp_vec.x / 2.f;
-    //     points[i].y -= tmp_vec.y / 2.f;
-    // }
-
-    // sfBool is_graph_drag = sfFalse;
-
-    // sfVector2i shift = {0, 0};
-
-    // sfVector2i last_shift = {0, 0};
-
-    while (sfRenderWindow_isOpen(plt.window)) {
-        while (sfRenderWindow_pollEvent(plt.window, plt.event)) {
-            if (plt.event->type == sfEvtClosed)
-                sfRenderWindow_close(plt.window);
-        }
-
-        if (sfMouse_isButtonPressed(sfMouseLeft)) {
-            if (!is_graph_drag) {
-                is_graph_drag = sfTrue;
-                last_shift = sfMouse_getPosition(plt.window);
-            }
-            else {
-                sfVector2i mouse_vec = sfMouse_getPosition(plt.window);
-                shift.x += mouse_vec.x - last_shift.x;
-                shift.y += mouse_vec.y - last_shift.y;
-                last_shift = mouse_vec;
-            }
-        } else
-            is_graph_drag = sfFalse;
-
-        sfRenderWindow_clear(plt.window, plt.theme->bg);
-        for (size_t i = 0; i < ARRAY_LENGTH(points); ++i) {
-            sfCircleShape *current_pts = sfCircleShape_create();
-            sfCircleShape_setFillColor(current_pts, plt.theme->pt);
-            sfCircleShape_setRadius(current_pts, plt.theme->radius);
-            sfVector2f tmp_pts = points[i];
-            tmp_pts.x += shift.x;
-            tmp_pts.y += shift.y;
-            sfCircleShape_setPosition(current_pts, tmp_pts);
-            sfRenderWindow_drawCircleShape(plt.window, current_pts, NULL);
-            sfCircleShape_destroy(current_pts);
-        }
-        sfVertex line[] = {
-            {{0, shift.y + tmp_vec.y / 2}, plt.theme->axis},
-            {{tmp_vec.x, shift.y + tmp_vec.y / 2}, plt.theme->axis}
-        };
-
-        sfRenderWindow_drawPrimitives(plt.window, line, 2, sfLines, NULL);
-        sfVertex line2[] = {
-            {{tmp_vec.x / 2 + shift.x, 0}, plt.theme->axis},
-            {{tmp_vec.x / 2 + shift.x, tmp_vec.y}, plt.theme->axis}
-        };
-
-        sfRenderWindow_drawPrimitives(plt.window, line2, 2, sfLines, NULL);
-        sfRenderWindow_display(plt.window);
-    }
 
     sfRenderWindow_destroy(plt.window);
 
+#if 0
 #endif
     return 0;
 }
