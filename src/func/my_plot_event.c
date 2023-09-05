6b055977fb10ed900d6fe05d170a8782e7f69abe
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 14:46:51 +0200

    hard reset

--- src/func/my_plot_event.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/my.h"
-
-void my_plot_handle_event(my_plot_t *plt)
-{
-    while (sfRenderWindow_pollEvent(plt->window, plt->event)) {
-        if (plt->event->type == sfEvtClosed)
-            sfRenderWindow_close(plt->window);
-    }
-}
