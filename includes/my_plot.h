595c5d6b11700e15447724368d8310d35127990c
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 21:01:41 +0200

    add some features to datatypes

--- includes/my_plot.h
+++ includes/my_plot.h
@@ -4,17 +4,31 @@
 #include "SFML/Graphics.h"
 #include "my_math.h"
 
+typedef union {
+    struct {
+        sfColor bg;
+        sfColor axis;
+    };
+    struct {
+        sfColor point;
+        uint32_t radius;
+    };
+} my_theme_t;
+
 typedef struct {
     double *xs;
     double *ys;
     uint32_t pts_n;
     uint32_t max_pts_n;
+    my_theme_t *th;
 } my_graph_t;
 
 typedef struct {
     sfRenderWindow *window;
     sfEvent *event;
     char *title;
+    my_theme_t *th;
+    my_graph_t **gs;
 } my_plot_t;
 
 #endif
