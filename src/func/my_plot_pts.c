7f564c3ab0c8ce94582bf266cfcd311e946f7cc4
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 00:20:48 +0200

    buig fix

--- src/func/my_plot_pts.c
+++ src/func/my_plot_pts.c
@@ -5,14 +5,12 @@ void my_plot_pts(my_plot_t *plt, my_graph_t *g)
     sfVector2u window_size = sfRenderWindow_getSize(plt->window);
 
     for (uint32_t i = 0; i < g->pts_n; ++i) {
+        double rad = g->th->radius;
         sfVector2f pos = {
-            g->xs[i] * (plt->ratio.x + plt->zoom.x) + window_size.x / 2 -\
-                                                            10 + plt->shift.x,
-            window_size.y - g->ys[i] * (plt->ratio.y + plt->zoom.y) - 10 -\
-                                            window_size.y / 2 + plt->shift.y
+            g->xs[i] * (plt->ratio.x + plt->zoom.x) + window_size.x / 2 - rad + plt->shift.x,
+            window_size.y - g->ys[i] * (plt->ratio.y + plt->zoom.y) - rad - window_size.y / 2 + plt->shift.y
         };
-        if (pos.x <= -20 || pos.x >= window_size.x || pos.y <= -20 ||\
-                                                    pos.y >= window_size.y)
+        if (pos.x <= -2 * rad || pos.x >= window_size.x || pos.y <= -14 || pos.y >= window_size.y)
             continue;
         sfCircleShape *pts = sfCircleShape_create();
         sfCircleShape_setPosition(pts, pos);
