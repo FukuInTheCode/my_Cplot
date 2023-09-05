a0b65ae561a4aeb6c1fe380e19f7b9d578a1268d
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 23:07:19 +0200

    add shift handling

---
+++ src/func/my_plot_shift.c
@@ -0,0 +1,33 @@
+#include "../../includes/my.h"
+
+void my_plot_handle_shift(my_plot_t *plt)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyLeft)) {
+        if (sfKeyboard_isKeyPressed(sfKeyLControl))
+            plt->shift.x -= 10;
+        else
+            plt->shift.x -= 1;
+    } else if (sfKeyboard_isKeyPressed(sfKeyRight)) {
+        if (sfKeyboard_isKeyPressed(sfKeyLControl))
+            plt->shift.x += 10;
+        else
+            plt->shift.x += 1;
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyUp)) {
+        if (sfKeyboard_isKeyPressed(sfKeyLControl))
+            plt->shift.y -= 10;
+        else
+            plt->shift.y -= 1;
+    } else if (sfKeyboard_isKeyPressed(sfKeyDown)) {
+        if (sfKeyboard_isKeyPressed(sfKeyLControl))
+            plt->shift.y += 10;
+        else
+            plt->shift.y += 1;
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyO)) {
+        plt->shift.x = 0;
+        plt->shift.y = 0;
+        plt->zoom.x = 0;
+        plt->zoom.y = 0;
+    }
+}
--- src/func/my_plot_show.c
+++ src/func/my_plot_show.c
@@ -13,37 +13,6 @@ void my_plot_show(my_plot_t *plt)
 
         my_plot_handle_zoom(plt);
 
-        // keyboard moving handling
-        if (sfKeyboard_isKeyPressed(sfKeyLeft)) {
-            if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                plt->shift.x -= 10;
-            else
-                plt->shift.x -= 1;
-        } else if (sfKeyboard_isKeyPressed(sfKeyRight)) {
-            if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                plt->shift.x += 10;
-            else
-                plt->shift.x += 1;
-        }
-
-        if (sfKeyboard_isKeyPressed(sfKeyUp)) {
-            if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                plt->shift.y -= 10;
-            else
-                plt->shift.y -= 1;
-        } else if (sfKeyboard_isKeyPressed(sfKeyDown)) {
-            if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                plt->shift.y += 10;
-            else
-                plt->shift.y += 1;
-        }
-        if (sfKeyboard_isKeyPressed(sfKeyO)) {
-            plt->shift.x = 0;
-            plt->shift.y = 0;
-            plt->zoom.x = 0;
-            plt->zoom.y = 0;
-        }
-
         // axis plotting
         sfVertex line2[] = {
             {{0, window_size.y / 2 + plt->shift.y}, plt->th->axis, {0, 0}},
