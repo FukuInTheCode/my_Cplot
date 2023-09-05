cee77027b7f7342ff872d74ad32428530a26a816
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 22:56:34 +0200

    chnage in the soh

--- src/func/my_plot_show.c
+++ src/func/my_plot_show.c
@@ -5,7 +5,7 @@ void my_plot_show(my_plot_t *plt)
     sfVector2u window_size = sfRenderWindow_getSize(plt->window);
     while (sfRenderWindow_isOpen(plt->window)) {
 
-        // plotting
+        my_plot_handle_events(plt);
 
         sfRenderWindow_clear(plt->window, plt->th->bg);
 
