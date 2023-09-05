70168d46939d832d5c48382403799c2ca2393b03
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 01:07:18 +0200

    add func pplot

--- src/func/my_plot_func.c
+++ src/func/my_plot_func.c
@@ -18,5 +18,17 @@ void my_plot_func(my_plot_t *plt, my_graph_t *g)
 
 void my_plot_func2(my_plot_t *plt, my_graph_t *g)
 {
-    // ...
+    sfVector2u window_size = sfRenderWindow_getSize(plt->window);
+    double x_i = (g->th->radius - plt->shift.x - window_size.x / 2) /\
+                                            (plt->ratio.x + plt->zoom.x);
+    double x_end = (g->th->radius - plt->shift.x + window_size.x / 2) /\
+                                            (plt->ratio.x + plt->zoom.x);
+    double x_steps = (x_end - x_i) / g->max_pts_n;
+    g->pts_n = 0;
+    for (; x_i < x_end && g->pts_n < g->max_pts_n; x_i += x_steps) {
+        g->xs[g->pts_n] = x_i;
+        g->ys[g->pts_n] = g->f(x_i, g->params);
+        (g->pts_n)++;
+    }
 }
+
