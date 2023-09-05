60887df993716f14ff9aba5f548d41cc8cacc73d
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 23:17:59 +0200

    change i nshwo

--- src/func/my_plot_show.c
+++ src/func/my_plot_show.c
@@ -1,5 +1,12 @@
 #include "../../includes/my.h"
 
+static handling_shit(my_plot_t *plt)
+{
+    my_plot_handle_mouse(plt);
+    my_plot_handle_zoom(plt);
+    my_plot_handle_shift(plt);
+}
+
 void my_plot_show(my_plot_t *plt)
 {
     sfVector2u window_size = sfRenderWindow_getSize(plt->window);
@@ -9,17 +16,11 @@ void my_plot_show(my_plot_t *plt)
 
         sfRenderWindow_clear(plt->window, plt->th->bg);
 
-        my_plot_handle_mouse(plt);
-
-        my_plot_handle_zoom(plt);
-
-        my_plot_handle_shift(plt);
-
         my_plot_axis(plt);
 
         // pts plotting
-        for (uint32_t j = 0; j < plt->gs_n; ++j) {
-        }
+        for (uint32_t j = 0; j < plt->gs_n; ++j)
+            my_plot_pts(plt , plt->gs[j]);
 
         // end plotting
         sfRenderWindow_display(plt->window);
