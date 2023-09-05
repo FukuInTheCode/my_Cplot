5ebc5ecdc6d20039d5ab75a0d0e4ef52c6215fbc
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 21:18:54 +0200

    hard code now use the datatype

--- src/main.c
+++ src/main.c
@@ -10,133 +10,140 @@ int main(void)
     double xs[] = { 0, 2, -1, 0, 1, 2, 3, 4 };
     double ys[] = { 0, 3, 2, 3, 1, 2, 3, 4 };
 
+    my_theme_t plt_th = {
+        .bg = sfBlack,
+        .axis = sfWhite
+    };
+
     sfEvent event;
-    my_plot_t plt = {.title = "test", .event = &event};
+    my_plot_t plt = {
+        .title = "test",
+        .event = &event,
+        .th = &plt_th,
+        .shift.x = 0,
+        .shift.y = 0,
+        .mouse_save.x = 0,
+        .mouse_save.y = 0,
+        .zoom.x = 0,
+        .zoom.y = 0
+    };
 
     // create window
     sfVideoMode mode = {1000, 1000, 32};
-    plt.window = sfRenderWindow_create(mode, title, sfDefaultStyle, NULL);
-
-    plt.shift = {0, 0};
-
-    sfVector2i mouse_vec_save = {0, 0};
-
-    bool is_pressed = false;
+    plt.window = sfRenderWindow_create(mode, plt.title, sfDefaultStyle, NULL);
 
     // calc ratio
-    sfVector2u window_size = sfRenderWindow_getSize(window);
+    sfVector2u window_size = sfRenderWindow_getSize(plt.window);
 
-    sfVector2f ratio = {
-        (window_size.x - 10) / (my_max(xs, n) - my_min(xs, n) + 0.1) / 2,
-        (window_size.y - 10) / (my_max(ys, n) - my_min(ys, n) + 0.1) / 2
-    };
+    plt.ratio.x = (window_size.x - 10) / (my_max(xs, n) - my_min(xs, n) + 0.1) / 2;
+    plt.ratio.y = (window_size.y - 10) / (my_max(ys, n) - my_min(ys, n) + 0.1) / 2;
 
 
     // show
-    while (sfRenderWindow_isOpen(window)) {
+    while (sfRenderWindow_isOpen(plt.window)) {
         // event handling
-        while (sfRenderWindow_pollEvent(window, &event)) {
-            if (event.type == sfEvtClosed) {
-                sfRenderWindow_close(window);
+        while (sfRenderWindow_pollEvent(plt.window, plt.event)) {
+            if (plt.event->type == sfEvtClosed) {
+                sfRenderWindow_close(plt.window);
             }
         }
 
         // plotting
 
-        sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_clear(plt.window, plt.th->bg);
 
         // mouse handling
         if (sfMouse_isButtonPressed(sfMouseLeft)) {
-            if (is_pressed) {
+            if (plt.is_pressed) {
                 sfVector2i tmp = sfMouse_getPosition(NULL);
-                shift.x += tmp.x - mouse_vec_save.x;
-                shift.y += tmp.y - mouse_vec_save.y;
-                mouse_vec_save = tmp;
+                plt.shift.x += tmp.x - plt.mouse_save.x;
+                plt.shift.y += tmp.y - plt.mouse_save.y;
+                plt.mouse_save = tmp;
             } else {
-                mouse_vec_save = sfMouse_getPosition(NULL);
-                is_pressed = true;
+                plt.mouse_save = sfMouse_getPosition(NULL);
+                plt.is_pressed = true;
             }
-        } else if (is_pressed)
-            is_pressed = false;
+        } else if (plt.is_pressed)
+            plt.is_pressed = false;
 
         // zoom handling
         if (sfKeyboard_isKeyPressed(sfKeyA)) {
-            ratio.x += 1;
-            ratio.y += 1;
+            plt.ratio.x += 1;
+            plt.ratio.y += 1;
         } else if (sfKeyboard_isKeyPressed(sfKeyZ)) {
-            ratio.x = my_max_between(ratio.x - 1, 0);
-            ratio.y = my_max_between(ratio.y - 1, 0);;
+            plt.ratio.x = my_max_between(plt.ratio.x - 1, 0);
+            plt.ratio.y = my_max_between(plt.ratio.y - 1, 0);;
         }
 
         // keyboard moving handling
         if (sfKeyboard_isKeyPressed(sfKeyLeft)) {
             if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                shift.x -= 10;
+                plt.shift.x -= 10;
             else
-                shift.x -= 1;
+                plt.shift.x -= 1;
         } else if (sfKeyboard_isKeyPressed(sfKeyRight))
             if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                shift.x += 10;
+                plt.shift.x += 10;
             else
-                shift.x += 1;
+                plt.shift.x += 1;
 
         if (sfKeyboard_isKeyPressed(sfKeyUp)) {
             if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                shift.y -= 10;
+                plt.shift.y -= 10;
             else
-                shift.y -= 1;
+                plt.shift.y -= 1;
         } else if (sfKeyboard_isKeyPressed(sfKeyDown))
             if (sfKeyboard_isKeyPressed(sfKeyLControl))
-                shift.y += 10;
+                plt.shift.y += 10;
             else
-                shift.y += 1;
+                plt.shift.y += 1;
 
         if (sfKeyboard_isKeyPressed(sfKeyO)) {
-            shift.x = 0;
-            shift.y = 0;
-            ratio.x = (window_size.x - 10) / (my_max(xs, n) - my_min(xs, n) + 0.1) / 2;
-            ratio.y = (window_size.y - 10) / (my_max(ys, n) - my_min(ys, n) + 0.1) / 2;
+            plt.shift.x = 0;
+            plt.shift.y = 0;
+            plt.ratio.x = (window_size.x - 10) / (my_max(xs, n) - my_min(xs, n) + 0.1) / 2;
+            plt.ratio.y = (window_size.y - 10) / (my_max(ys, n) - my_min(ys, n) + 0.1) / 2;
         }
 
         // axis plotting
         sfVertex line2[] = {
-            {{0, window_size.y / 2 + shift.y}, sfWhite},
-            {{window_size.x, window_size.y / 2 + shift.y}, sfWhite}
+            {{0, window_size.y / 2 + plt.shift.y}, plt.th->axis},
+            {{window_size.x, window_size.y / 2 + plt.shift.y}, plt.th->axis}
         };
 
         sfVertex line[] = {
-            {{window_size.x / 2 + shift.x, 0}, sfWhite},
-            {{window_size.x / 2 + shift.x, window_size.y}, sfWhite}
+            {{window_size.x / 2 + plt.shift.x, 0}, plt.th->axis},
+            {{window_size.x / 2 + plt.shift.x, window_size.y}, plt.th->axis}
         };
 
         if (line[0].position.x > 0 && line[0].position.x < window_size.x)
-            sfRenderWindow_drawPrimitives(window, line, 2, sfLines, NULL);
+            sfRenderWindow_drawPrimitives(plt.window, line, 2, sfLines, NULL);
 
 
         if (line2[0].position.y > 0 && line2[0].position.y < window_size.y)
-            sfRenderWindow_drawPrimitives(window, line2, 2, sfLines, NULL);
+            sfRenderWindow_drawPrimitives(plt.window, line2, 2, sfLines, NULL);
 
         // pts plotting
         for (uint32_t i = 0; i < n; ++i) {
             sfCircleShape *pts = sfCircleShape_create();
             sfVector2f pos = {
-                xs[i] * ratio.x + window_size.x / 2 - 10 + shift.x,
-                window_size.y - ys[i] * ratio.y - 10 - window_size.y / 2 + shift.y
+                xs[i] * plt.ratio.x + window_size.x / 2 - 10 + plt.shift.x,
+                window_size.y - ys[i] * plt.ratio.y - 10 - window_size.y / 2 + plt.shift.y
             };
             if (pos.x <= -20 || pos.x >= window_size.x || pos.y <= -20 || pos.y >= window_size.y)
                 continue;
             sfCircleShape_setPosition(pts, pos);
             sfCircleShape_setRadius(pts, 10);
             sfCircleShape_setFillColor(pts, sfRed);
-            sfRenderWindow_drawCircleShape(window, pts, NULL);
+            sfRenderWindow_drawCircleShape(plt.window, pts, NULL);
             sfCircleShape_destroy(pts);
         }
 
         // end plotting
-        sfRenderWindow_display(window);
+        sfRenderWindow_display(plt.window);
 
     }
     // free
-    sfRenderWindow_destroy(window);
+    sfRenderWindow_destroy(plt.window);
     return 0;
 }
