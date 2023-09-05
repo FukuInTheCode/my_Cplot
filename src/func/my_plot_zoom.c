9a5c6b38c381b93b6402a044fa0cd1ff3c6dd9be
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 23:40:03 +0200

    zoom changes

--- src/func/my_plot_zoom.c
+++ src/func/my_plot_zoom.c
@@ -6,8 +6,7 @@ void my_plot_handle_zoom(my_plot_t *plt)
         plt->zoom.x += 1;
         plt->zoom.y += 1;
     } else if (sfKeyboard_isKeyPressed(sfKeyZ)) {
-        plt->zoom.x = my_max_between(plt->zoom.x - 1, -plt->ratio.x);
-        plt->zoom.y = my_max_between(plt->zoom.y - 1, -plt->ratio.y);
+        plt->zoom.x = my_max_between(plt->zoom.x - 1, -plt->ratio.x + 1e-2);
+        plt->zoom.y = my_max_between(plt->zoom.y - 1, -plt->ratio.y + 1e-2);
     }
-
 }
