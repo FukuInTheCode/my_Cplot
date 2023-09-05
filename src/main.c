7cb449319725c7412432503aa54a420840ef3e3d
FukuInTheCode <tomc.code@gmail.com>
2023-09-05 22:26:15 +0200

    end of the hard code

--- src/main.c
+++ src/main.c
@@ -6,10 +6,24 @@ int main(void)
     // main
 
     uint32_t n = 8;
+    uint32_t n2 = 3;
 
     double xs[] = { 0, 2, -1, 0, 1, 2, 3, 4 };
     double ys[] = { 0, 3, 2, 3, 1, 2, 3, 4 };
+    double xs2[] = { 2, 3, 4 };
+    double ys2[] = { 0, 3, 2 };
 
+    my_theme_t g2_th = {
+        .point = sfYellow,
+        .radius = 7
+    };
+
+    my_graph_t g2 = {
+        .xs = xs2,
+        .ys = ys2,
+        .pts_n = n2,
+        .th = &g2_th
+    };
     my_theme_t g1_th = {
         .point = sfRed,
         .radius = 10
@@ -23,7 +37,8 @@ int main(void)
     };
 
     my_graph_t *gs[] = {
-        &g1
+        &g1,
+        &g2
     };
 
     my_theme_t plt_th = {
@@ -42,7 +57,7 @@ int main(void)
         .mouse_save.y = 0,
         .zoom.x = 0,
         .zoom.y = 0,
-        .gs_n = 1,
+        .gs_n = 2,
         .gs = gs
     };
 
@@ -55,7 +70,6 @@ int main(void)
 
     plt.ratio.x = (window_size.x - 10) / (my_max(xs, n) - my_min(xs, n) + 0.1) / 2;
     plt.ratio.y = (window_size.y - 10) / (my_max(ys, n) - my_min(ys, n) + 0.1) / 2;
-
 
     // show
     while (sfRenderWindow_isOpen(plt.window)) {
