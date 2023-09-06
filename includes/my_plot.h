7ff1101daf2c670f84f69a1da2135d39a0e6ec1a
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 14:26:08 +0200

    change in header

--- includes/my_plot.h
+++ includes/my_plot.h
@@ -64,5 +64,6 @@ void my_plot_axis(my_plot_t *plt);
 void my_plot_pts(my_plot_t *plt, my_graph_t *g);
 void my_plot_func(my_plot_t *plt, my_graph_t *g);
 void my_plot_func2(my_plot_t *plt, my_graph_t *g);
+void my_plot_free(my_plot_t *plt);
 
 #endif
