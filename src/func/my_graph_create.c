8963196b14b147600a8ac980192280fb0b4f398c
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 14:55:16 +0200

    add create f and changes

--- src/func/my_graph_create.c
+++ src/func/my_graph_create.c
@@ -1,11 +1,24 @@
 #include "../../includes/my.h"
 
-void my_graph_create_pts(my_graph_t *g, uint32_t n, my_theme_t *th)
+static void create(my_graph_t *g, uint32_t n, my_theme_t *th)
 {
-    g->type = points;
     g->xs = calloc(double, n);
     g->ys = calloc(double, n);
     g->pts_n = n;
     g->max_pts_n = n;
     g->th = th;
+}
+
+void my_graph_create_pts(my_graph_t *g, uint32_t n, my_theme_t *th)
+{
+    g->type = points;
+    create(g, n, th);
+}
+
+void my_graph_create_f(my_graph_t *g, uint32_t n, my_theme_t *th, func f)
+{
+    g->type = function;
+    create(g, n, th);
+    g->pts_n = 0;
+    g->f = f;
 }
