20d318b982166f0c876fe13a4812ee9dc7555541
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 15:19:17 +0200

    change in zoom func (add a greater speed)

--- src/func/my_plot_zoom.c
+++ src/func/my_plot_zoom.c
@@ -1,12 +1,25 @@
 #include "../../includes/my.h"
 
+
+
 void my_plot_handle_zoom(my_plot_t *plt)
 {
     if (sfKeyboard_isKeyPressed(sfKeyA)) {
-        plt->zoom.x += 1;
-        plt->zoom.y += 1;
+        if (sfKeyboard_isKeyPressed(sfKeyLShift)) {
+            plt->zoom.x += 10;
+            plt->zoom.y += 10;
+        } else {
+            plt->zoom.x += 1;
+            plt->zoom.y += 1;
+        }
+
     } else if (sfKeyboard_isKeyPressed(sfKeyZ)) {
-        plt->zoom.x = my_max_between(plt->zoom.x - 1, -plt->ratio.x + 1e-2);
-        plt->zoom.y = my_max_between(plt->zoom.y - 1, -plt->ratio.y + 1e-2);
+        if (sfKeyboard_isKeyPressed(sfKeyLShift)) {
+            plt->zoom.x = my_max_between(plt->zoom.x - 10, -plt->ratio.x + 1e-2);
+            plt->zoom.y = my_max_between(plt->zoom.y - 10, -plt->ratio.y + 1e-2);
+        } else {
+            plt->zoom.x = my_max_between(plt->zoom.x - 1, -plt->ratio.x + 1e-2);
+            plt->zoom.y = my_max_between(plt->zoom.y - 1, -plt->ratio.y + 1e-2);
+        }
     }
 }
