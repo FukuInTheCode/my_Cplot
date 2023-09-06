35d6cc2d36145bdd04bd4f8f93efcd6f17676201
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 15:34:03 +0200

    memory gestion of the graph

--- src/func/my_plot_free.c
+++ src/func/my_plot_free.c
@@ -4,6 +4,12 @@ void my_plot_free(my_plot_t *plt)
 {
     sfRenderWindow_destroy(plt->window);
 
-    if(plt->gs_n > 0)
-        free(plt->gs);
+    if(plt->gs_n == 0)
+        return;
+
+    for (uint32_t i = 0; i < plt->gs_n; ++i) {
+        free(plt->gs[i]->xs);
+        free(plt->gs[i]->ys);
+    }
+    free(plt->gs);
 }
