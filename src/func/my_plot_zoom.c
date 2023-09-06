ed2b52cfc0c337770a65fb2140735cf2fb9bf883
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 15:23:33 +0200

    sepearate dezoom and zoom

--- src/func/my_plot_zoom.c
+++ src/func/my_plot_zoom.c
@@ -1,8 +1,6 @@
 #include "../../includes/my.h"
 
-
-
-void my_plot_handle_zoom(my_plot_t *plt)
+static void handle_zoom(my_plot_t *plt)
 {
     if (sfKeyboard_isKeyPressed(sfKeyA)) {
         if (sfKeyboard_isKeyPressed(sfKeyLShift)) {
@@ -12,14 +10,26 @@ void my_plot_handle_zoom(my_plot_t *plt)
             plt->zoom.x += 1;
             plt->zoom.y += 1;
         }
+    }
+}
 
-    } else if (sfKeyboard_isKeyPressed(sfKeyZ)) {
+static void handle_dezoom(my_plot_t *plt)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyZ)) {
         if (sfKeyboard_isKeyPressed(sfKeyLShift)) {
-            plt->zoom.x = my_max_between(plt->zoom.x - 10, -plt->ratio.x + 1e-2);
-            plt->zoom.y = my_max_between(plt->zoom.y - 10, -plt->ratio.y + 1e-2);
+            plt->zoom.x -= 10;
+            plt->zoom.y -= 10;
         } else {
-            plt->zoom.x = my_max_between(plt->zoom.x - 1, -plt->ratio.x + 1e-2);
-            plt->zoom.y = my_max_between(plt->zoom.y - 1, -plt->ratio.y + 1e-2);
+            plt->zoom.x -= 1;
+            plt->zoom.y -= 1;
         }
     }
+    plt->zoom.x = my_max_between(plt->zoom.x, -plt->ratio.x + 1e-2);
+    plt->zoom.y = my_max_between(plt->zoom.y, -plt->ratio.y + 1e-2);
+}
+
+void my_plot_handle_zoom(my_plot_t *plt)
+{
+    handle_dezoom(plt);
+    handle_zoom(plt);
 }
